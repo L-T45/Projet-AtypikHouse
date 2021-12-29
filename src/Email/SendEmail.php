73f0ac681fdd58dd7195caa48750d5c4ccd45ab6
L-T45 <57352534+L-T45@users.noreply.github.com>
2021-12-29 21:26:33 +0100

    Test send mail successful but one bug again

--- src/Email/SendEmail.php
+++ src/Email/SendEmail.php
@@ -2,62 +2,22 @@
 
 namespace App\Email;
 
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Mailer\Bridge\Google\Smtp\GmailTransport;
-use Symfony\Component\Mime\TemplatedEmail;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
-class SendEmailCommand extends Command
-{
-	protected static $defaultName = 'send:email';
-	private $mailer;
+class SendEmail extends AbstractController {
 
-	public function __construct()
-	{
-		$this->mailer = $mailer;
-        
-		parent::__construct();
-	}
+    public function sendEmail(MailerInterface $mailer): Response{
+        $email = (new Email())
+            ->from('atypikhouse.communication@gmail.com')
+            ->to('atypikhouse.communication@gmail.com')
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!');
+            //->->html('<p>See Twig integration for better HTML integration!</p>');
 
-	protected function configure()
-	{
-		$this
-			->setDescription('Command for send self email')
-		;
-	}
-    
-	protected function SendMailTest(InputInterface $input, OutputInterface $output)
-	{
-		$output->writeln([
-			'Command Send Self Email',
-			'============'
-		]);
+        $mailer->send($email);
 
-		// If you want use htmlTemplate and context function you need use TemplatedEmail
-		// instead of Email object 
-		$email = (new TemplatedEmail())
-			->from('atypikhouse.communication@gmail.com')
-			->to('atypikhouse.communication@gmail.com')
-			//->cc('exemple@mail.com')
-			//->bcc('exemple@mail.com')
-			//->replyTo('test42@gmail.com')
-			//->priority(Email::PRIORITY_HIGH)
-			->subject('I love Me')
-			// If you want use text mail only
-			->text('Lorem ipsum...')
-			// If you want use raw html
-			->html('<h1>Hello World</h1> <p>...</p>')
-			// if you want use template from your twig file
-			// template/emails/registration.html.twig
-			->htmlTemplate('emails/registration.html.twig')
-			// with param 
-			->context([
-				'username' => 'John',
-			])
-			;
-		$this->mailer->send($email);
-
-		$output->writeln('Successful you send a self email');
-	}
+    }
 }
