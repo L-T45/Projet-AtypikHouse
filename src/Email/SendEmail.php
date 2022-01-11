247c5b566e85264893a861dbde6b168edcc0a631
L-T45 <57352534+L-T45@users.noreply.github.com>
2022-01-11 19:07:33 +0100

    delete asserts in SendEmail.php

--- src/Email/SendEmail.php
+++ src/Email/SendEmail.php
@@ -19,68 +19,11 @@ use Symfony\Component\Validator\Constraints as Assert;
 class SendEmail extends AbstractController {
 
     // Pour le formulaire de contact
-
-    /** 
-     * @Assert\Type("string") 
-     * @Assert\Length(
-     *      min = 2,
-     *      max = 60,
-     *      minMessage = "Your first name must be at least {{ limit }} characters long",
-     *      maxMessage = "Your first name cannot be longer than {{ limit }} characters"
-     * )
-     */
     public $forname;
-
-    /** 
-     * @Assert\Type("string") 
-     * @Assert\Length(
-     *      min = 2,
-     *      max = 60,
-     *      minMessage = "Your last name must be at least {{ limit }} characters long",
-     *      maxMessage = "Your last name cannot be longer than {{ limit }} characters"
-     * )
-     */
     public $lastname;
-
-    /** 
-     * @Assert\Type("integer") 
-     * @Assert\Length(
-     *      max = 15,
-     *      maxMessage = "Your phone cannot be longer than {{ limit }} characters"
-     * )
-     */ 
     public $phone;
-
-    /** 
-     * @Assert\Email( * message = "l'adresse mail '{{ value }}' n'est pas une adresse mail valide ." * ) 
-     * @Assert\Regex (?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])
-     * @Assert\Length(
-     *      min = 6,
-     *      max = 80,
-     *      minMessage = "Your email must be at least {{ limit }} characters long",
-     *      maxMessage = "Your email cannot be longer than {{ limit }} characters"
-     * )
-     */
-    public $email;
-
-    /** 
-     * @Assert\Type("string") 
-     * @Assert\Length(
-     *      max = 60,
-     *      maxMessage = "Your objet cannot be longer than {{ limit }} characters"
-     * )
-     */    
+    public $email; 
     public $objet;
-
-    /** 
-     * @Assert\Type("string") 
-     * @Assert\Length(
-     *      min = 6,
-     *      max = 800,
-     *      minMessage = "Your message must be at least {{ limit }} characters long",
-     *      maxMessage = "Your message name cannot be longer than {{ limit }} characters"
-     * )
-     */
     public $message;
 
     public function sendEmailFormContact(MailerInterface $mailer, Request $request): Response{
