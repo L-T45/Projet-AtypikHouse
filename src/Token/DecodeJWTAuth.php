dce7eba7614c425be556ab95f3bbe3d730a2a3b9
L-T45 <57352534+L-T45@users.noreply.github.com>
2022-01-16 02:33:04 +0100

    begin to take the username to search user's informations

--- src/Token/DecodeJWTAuth.php
+++ src/Token/DecodeJWTAuth.php
@@ -10,6 +10,17 @@ use Lexik\Bundle\JWTAuthenticationBundle\Encoder\JWTEncoderInterface;
 class DecodeJWTAuth extends AbstractController
 {
     private $token;
+    private $username;
+
+    // pour ne concerver que ce que l'on souhaite après avoir serialize une variable de type array 
+    public function cutChaine($string, $start, $end){
+        $string = ' ' . $string;   
+        $ini = strpos($string, $start);  
+        if ($ini == 0) return '';   
+        $ini += strlen($start);  
+        $len = strpos($string, $end, $ini) - $ini;
+        return substr($string, $ini, $len);   
+    }
 
     public function decodeJwtToken(JWTEncoderInterface $jwtManager, Request $request)
     {   
@@ -17,7 +28,18 @@ class DecodeJWTAuth extends AbstractController
         $data = json_decode( $request->getContent(), true );
         $token = $data['token'];
         $decodedJwtToken = $jwtManager->decode($token);
+        //dd($decodedJwtToken);
 
-        
+        if($decodedJwtToken =! ''){
+            $username = serialize($jwtManager->decode($token));
+            //dd($username);
+            $username = $this->cutChaine($username, '"username";s:', ';'); 
+            $username = $this->cutChaine($username, ':"', '"'); 
+            //dd($username);
+            if($username =! ''){
+                
+            }
+        }
+
     }
 }
