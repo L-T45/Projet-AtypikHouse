78212e1a4bd9cf708590a67c0a13d12f05741dfc
Lionel Broutin <lionelbroutin@hotmail.fr>
2022-02-06 00:05:16 +0100

    created attribute

---
+++ src/Controller/AttributeController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Attributes;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+
+class AttributeController
+{
+
+
+    /**
+     * @Route("/api/dashboard/admin/attributes/create", methods={"POST"})
+     */
+    public function createAttribute(Request $request, EntityManagerInterface $manager)
+    {
+
+        $post = json_decode($request->getContent(), true);
+
+        $categoryRef = $manager->getReference("App\Entity\Categories", $post["categories"]);
+        $newAtttibute = new Attributes();
+        $newAtttibute->setTitle($post["title"]);
+        $newAtttibute->setCategories($categoryRef);
+        $newAtttibute->setResponseType($post['response_type']);
+        $newAtttibute->setRequired($post["required"]);
+
+        $manager->persist($newAtttibute);
+        $manager->flush();
+        return new JsonResponse(["status" => 201, "message" => "Attribut créé avec succès !"]);
+    }
+}
--- src/Delete/DeleteAttribute.php
+++ src/Delete/DeleteAttribute.php
@@ -6,31 +6,32 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Entity\Attributes;
 use App\Repository\AttributesRepository;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;  
-use Symfony\Component\Mailer\MailerInterface; 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
 use App\Email\SendEmailModifyListAttributes;
 
-class DeleteAttribute extends AbstractController {
+class DeleteAttribute extends AbstractController
+{
 
     // Pour le formulaire de delete Attributes
     private $idAttributes;
     private $AttributeRepository;
 
-    public function DeleteAttribute(Request $request, AttributesRepository $AttributesRepository, SendEmailModifyListAttributes $SendEmail, MailerInterface $mailer): Response{
+    public function DeleteAttribute(Request $request, AttributesRepository $AttributesRepository, SendEmailModifyListAttributes $SendEmail, MailerInterface $mailer): Response
+    {
 
         $data = $request->query->get('id');
         $idAttributes = $data;
- 
+
         $this->AttributesRepository = $AttributesRepository;
         $findAttributesToDelete = $this->AttributesRepository->findByIdToDelete($idAttributes);
 
-            if($findAttributesToDelete =! []){
-                $SendEmail->DeleteAttributes($mailer, $request);
-                $response = new Response('Attribut supprimé',Response::HTTP_OK,['content-type' => 'application/json']);
-            }
-            else{  
-                $response = new Response("Une erreur est survenu lors de la suppression de l'attribut...",Response::HTTP_BAD_REQUEST,['content-type' => 'application/json']);     
-            }
+        if ($findAttributesToDelete = ![]) {
+            $response = new Response('Attribut supprimé', Response::HTTP_OK, ['content-type' => 'application/json']);
+            $SendEmail->DeleteAttributes($mailer, $request);
+        } else {
+            $response = new Response("Une erreur est survenu lors de la suppression de l'attribut...", Response::HTTP_BAD_REQUEST, ['content-type' => 'application/json']);
+        }
         return $response;
     }
 }
